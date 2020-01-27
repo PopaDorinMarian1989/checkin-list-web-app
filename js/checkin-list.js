9ce555e683a3b38d47138d2bbe3452e46c4ed993
popa dorin <dorinmarian89@gmail.com>
2020-01-27 20:39:36 +0200

    Finish UI

--- js/checkin-list.js
+++ js/checkin-list.js
@@ -1,33 +1,88 @@
 window.index = {
     API_BASE_URL: "http://localhost:8085",
+    getMembers: function () {
+        $.ajax({
+            url: index.API_BASE_URL + "/gymMember",
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
+            console.log(response);
+            index.displayMember(response);
+        });
+    },
 
-    createGymMember: function () {
-
+    createMember: function () {
         let firstNameValue = $("#firstName-field").val();
         let lastNameValue = $("#lastName-field").val();
-        let numberValue = $("#number-field").val();
+        let memberNumber = $("#nr-field").val();
 
         var requestBody = {
             firstName: firstNameValue,
             lastName: lastNameValue,
-            gymMembernumber: numberValue
+            gymMemberNumber: memberNumber,
         };
 
         $.ajax({
             url: index.API_BASE_URL + "/gymMember",
             method: "POST",
-            // MIME type
             contentType: "application/json",
             data: JSON.stringify(requestBody)
         }).done(function () {
-            //ToDoList.getItems();
+            index.getMembers();
         })
     },
+    markItemDone: function (id, checked) {
+        let requestBody = {
+            checked: checked
+        };
+
+        $.ajax({
+            url: index.API_BASE_URL + "gymMember/" + id,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+           index.getMembers()
+        })
+    },
+
+    deleteMember: function (id) {
+        $.ajax({
+            url: index.API_BASE_URL + "/gymMember/" + id,
+            method: "DELETE"
+        }).done(function () {
+            index.getMembers();
+
+        });
+    },
+    displayMember: function (gymMembers) {
+        var tableContent = '';
+
+        gymMembers.forEach(member => tableContent += index.getMemberHtml(member));
+
+        $("#index tbody").html(tableContent);
+    }
+    ,
+    getMemberHtml: function (gymMember) {
+
+        return `<tr>
+                <td><a href="#" onclick="index.deleteMember(${gymMember.id})" class="delete-item"><i class="far fa-trash-alt"></i>
+                </a></td>
+                <td>${gymMember.firstName}</td>
+                <td>${gymMember.lastName}</td>
+                <td>${gymMember.gymMemberNumber}</td>
+            </tr>`
+    },
     bindEvents: function () {
-        $("create-item-form").submit(function (event) {
+        $("#index.delete-item").click(function (event) {
             event.preventDefault();
-            index.createGymMember();
+        });
+        $("#create-item-form").submit(function (event) {
+            event.preventDefault();
+            index.createMember();
         });
     }
 };
+index.getMembers();
 index.bindEvents();
+
